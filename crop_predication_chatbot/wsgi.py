643639fee06f720cefeccb359747cb0844a47560
Bhaskar <bhaskarganesetti@gmail.com>
2026-04-07 12:25:51 +0530

    Fixed DJANGO_SETTINGS_MODULE name in wsgi.py and asgi.py

--- crop_predication_chatbot/wsgi.py
+++ crop_predication_chatbot/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recommend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crop_predication_chatbot.settings')
 
 application = get_wsgi_application()
